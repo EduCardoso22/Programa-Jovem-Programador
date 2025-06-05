3d7014926846bd4c4214f85a8c2ac0bbcff0df80
Eduardo Cardoso <eduardo.co22@aluno.ifsc.edu.br>
2025-06-05 16:35:52 -0300

    outra atividade facin

---
+++ Testar-Sistemas/exercicio.py
@@ -0,0 +1,13 @@
+#Variaveis
+
+custo = int
+vendas = int
+lucro = int
+
+#Calculo do lucro
+
+custo = 78000
+vendas = 100000
+lucro = vendas - custo
+
+print('O total de vendas foi de R$ 100000, com um custo de R$ 78000 e um lucro de R$',lucro)
