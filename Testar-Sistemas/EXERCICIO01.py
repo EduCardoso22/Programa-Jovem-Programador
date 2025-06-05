cc15967efb0b8ea9cacdb7b2b1d7d1b16c422723
Eduardo Cardoso <eduardo.co22@aluno.ifsc.edu.br>
2025-06-05 16:14:58 -0300

    Atividades python

---
+++ Testar-Sistemas/EXERCICIO01.py
@@ -0,0 +1,10 @@
+a=1 #valor inteiro
+b='0' #string
+m1=m2=m3=0 #variaveis iguais
+nome,turma='fabio',10
+
+print('o valor de a é',a) #print oq ta dentro da variável
+print('o nome do professor é',nome,'e a turma dele é a',turma)
+
+print(type(a))
+print(type(nome))
---
+++ Testar-Sistemas/operadores-matematicos.py
@@ -0,0 +1,33 @@
+n1 = 50
+n2 = 10
+n3 = 5
+total = int
+# operadores matemáticos
+
+total = n1 + n2 + n3
+print(total)
+
+total = n1 - n2 - n3
+print(total)
+
+total = n1 * n2 * n3
+print(total)
+
+total = n1 / n2 / n3
+print(total)
+
+total = n1 ** n3
+print(total)
+
+total = n1%n2
+print(total)
+
+# ordem de precedência
+# 1. Parênteses
+# 2. Potências 
+# 3. Multiplicação e Divisão
+# Soma e Subtração
+# 5. Esquerda para a Direita
+
+total = n1+n2*n2 #n2*n2 vai ser feito primeiro
+print(total)
